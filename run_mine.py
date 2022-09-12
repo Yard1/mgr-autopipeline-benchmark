4ecccb4dc819fcb56cd3d19a92342f751bb42624
Antoni Baum <antoni.baum@protonmail.com>
2022-09-12 09:30:18 +0000

    Fix
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- run_mine.py
+++ run_mine.py
@@ -9,6 +9,7 @@ from automl.search.automl import AutoML
 from ray.tune.progress_reporter import JupyterNotebookReporter
 import contextlib
 import ray
+import argparse
 
 import os
 os.environ["TUNE_DISABLE_AUTO_CALLBACK_LOGGERS"] = "1"
@@ -20,48 +21,55 @@ f1_weighted.__name__ = "f1_weighted"
 
 def run(data_idx, random_seed):
     print(f"!FITTING {data_idx}_{random_seed}")
-    ray.init(num_cpus=4)
-    X, y = prepare_data(data_idx, random_seed, True, preprocess=False, split=False)
-    y = y.astype("category")
-    if y.nunique() > 2:
+    ray.init(num_cpus=16)
+    X_train, X_test, y_train, y_test = prepare_data(data_idx, random_seed, True, preprocess=False)
+    y_train = y_train.astype("category")
+    y_test = y_test.astype("category")
+    if y_train.nunique() > 2:
         func = f1_weighted
         eval_metric = "f1_weighted"
+        scorer = f1_weighted
     else:
         func = f1_score
         eval_metric = "f1"
+        scorer = partial(f1_score, zero_division=0)
     am = AutoML(
         random_state=random_seed,
         level="common",
-        test_size=0.25,
+        test_size=0.1,
         target_metric=eval_metric,
         trainer_config={
             "secondary_level": "uncommon",
             "cache": True,
-            "early_stopping": False,
+            # "early_stopping": False,
             "return_test_scores_during_tuning": False,
-            "tuning_time": 120,
+            "tuning_time": 3600,
             "stacking_level": 0,
-            "tune_kwargs": {"max_concurrent": 1, "trainable_n_jobs": 2, "verbose": 0, "fail_fast": False, "raise_on_failed_trial": False}
+            "tune_kwargs": {"max_concurrent": 3, "trainable_n_jobs": 5, "verbose": 0, "fail_fast": False, "raise_on_failed_trial": False}
         },
         stacking_cv=5,
         cv=5,
     )
-    makedirs(f"./tmp/mine_{data_idx}_{random_seed}/", exist_ok=True)
-    with open(f"./tmp/mine_{data_idx}_{random_seed}/log.txt", 'w') as f:
-        with contextlib.redirect_stderr(f), contextlib.redirect_stdout(f):
-            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-                am.fit(X, y)
-    best_test = am.results_.iloc[0][f"Test {eval_metric}"]
-    best_single_test = am.score_pipeline(am.best_id_non_ensemble_)[eval_metric]
-    print(f"best_test {best_test} best_single_test {best_single_test}")
-    final_test = best_single_test if best_single_test > best_test else best_test
-    am.results_.to_csv(f"./tmp/mine_{data_idx}_{random_seed}/results.csv")
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        am.fit(X_train, y_train)
+    pred = am.best_pipeline_
     print(am.results_)
     print(f"!NUM_EVALUATED {data_idx}_{random_seed} {len(am.results_)}")
-    print(f"!RESULT {data_idx}_{random_seed} F1 score", final_test)
+    y_hat = pred.predict(X_test)
+    print(f"!RESULT {data_idx}_{random_seed} F1 score", scorer(y_test, y_hat))
     ray.shutdown()
     shutil.rmtree("/tmp/joblib", ignore_errors=True)
 
 if __name__ == "__main__":
-    for i in range(22, 39):
-        run(i, 1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "seed",
+        type=int,
+        help="seed to use")
+    args, _ = parser.parse_known_args()
+    random_seed = args.seed
+    for i in range(0, 39):
+        makedirs(f"./tmp/mine_{i}_{random_seed}/", exist_ok=True)
+        with open(f"./tmp/mine_{i}_{random_seed}/log.txt", 'w') as f:
+            with contextlib.redirect_stderr(f), contextlib.redirect_stdout(f):
+                run(i, random_seed)
